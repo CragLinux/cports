d805558f705d6e3f2a34e089718c649499f0b875
Michael John <michael@aka.ninja>
2026-09-10 08:03:40 -0400

    main/rpi-boot: update to 1.20260824 (fetch-verified for aarch64)

--- main/rpi-boot/template.py
+++ main/rpi-boot/template.py
@@ -1,14 +1,14 @@
 pkgname = "rpi-boot"
-pkgver = "1.20260521"
+pkgver = "1.20260824"
 pkgrel = 0
 archs = ["aarch64"]
-_gitrev = "09267f5354d40519d82fbd2193b9e211ec304055"
+_gitrev = "eef9c230a0e70e23a54be98f90d5237ab6e0b0fd"
 replaces = ["firmware-rpi<=20220905-r0"]
 pkgdesc = "Bootloader for Raspberry Pi"
 license = "custom:raspberry"
 url = "https://github.com/raspberrypi/firmware"
 source = f"{url}/archive/{_gitrev}.tar.gz"
-sha256 = "3ce9c9f4473776411385ea6b5df3ae94b3d52ccbabf09f3757d59f71f01b5b44"
+sha256 = "b20877cd01b81889ba07358ff519cf9bd7afe3133eea8ed2c7164fa8023122ce"
 options = ["!strip", "foreignelf", "execstack"]
 
 
